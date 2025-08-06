f1efb937556f72638a2b95bc6eb7c9b10dba3831
tangshoubin <tangshoubin@itiger.com>
2025-08-06 19:37:17 +0800

    update

--- nginx/main.lua
+++ nginx/main.lua
@@ -1,16 +1,26 @@
 local opts = {
     ssl_verify = "no",
-    redirect_uri = "http://localhost/cb",
-    discovery = "http://keycloak:8080/realms/demo/.well-known/openid-configuration",
-    client_id = "sample-app",
-    client_secret = "d59cc875-2546-4bc6-81a9-f34ceb82f662"
+    redirect_uri = "http://localhost/redirect_uri",
+    discovery = "https://sso-test.exodushk.com/.well-known/openid-configuration",
+    client_id = "headlamp-default-test",
+    client_secret = "d1515790ce6d0a31f3a2",
+    refresh_session_interval = 3600,
+    access_token_expires_in = 3600,
+    session_contents = {
+        id_token = true,
+        user = true,
+    },
 }
 
-local res, err = require("resty.openidc").authenticate(opts)
+local openidc = require("resty.openidc")
+openidc.set_logging(nil, { DEBUG = ngx.DEBUG })
+local res, err = openidc.authenticate(opts)
+-- ngx.log(ngx.ERR, "openidc.authenticate res: ", require("cjson").encode(res))
+
 if err then
     ngx.log(ngx.ERR, err)
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 ngx.req.set_header("X-USER", res.id_token.sub)
 
-ngx.var.pass="http://webapp:3000/"
+ngx.var.pass="http://127.0.0.1:3000"
